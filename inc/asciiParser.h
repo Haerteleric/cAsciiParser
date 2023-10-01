126fa62c0e6dda0aa9e3a598e894ac11c47733d3
Haerteleric <haertelgu@gmail.com>
2023-10-01 20:33:10 +0200

    initial commit

---
+++ inc/asciiParser.h
@@ -0,0 +1,387 @@
+/**
+ * Portable ASCII Parser template implementation
+ * 
+ * Author:    Haerteleric
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2023 Eric Härtel
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **/
+#include <stddef.h>
+#include <stdbool.h>
+
+#ifndef _ASCII_PARSER_CONTENT_TYPES_ENUM_DEFINED
+#define _ASCII_PARSER_CONTENT_TYPES_ENUM_DEFINED
+
+typedef enum asciiStringContentTypes_e
+{
+    ASCII_STRING_CONTENT_SIGNED_DEC_INT,
+    ASCII_STRING_CONTENT_UNSIGNED_DEC_INT,
+    ASCII_STRING_CONTENT_BINARY_LITERAL,
+    ASCII_STRING_CONTENT_HEX_LITERAL,
+    ASCII_STRING_CONTENT_BYTE_ARRAY,
+    ASCII_STRING_CONTENT_ALPHANUMERIC,
+    ASCII_STRING_CONTENT_UNDEFINED
+}asciiStringContentType_t;
+
+#endif
+
+
+#ifdef ASCII_PARSER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PARSER_STATIC_IMPLEMENTATION
+static
+#endif 
+bool ascii_isValidBinaryChar(char c)
+#ifdef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+    if ( (c < '0') || (c > '1') )
+    {
+        return false;
+    }
+
+    return true;
+}
+#endif // NOT(ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PARSER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PARSER_STATIC_IMPLEMENTATION
+static
+#endif 
+bool ascii_isValidDecimalChar(char c)
+#ifdef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+    if ( (c < '0') || (c > '9') )
+    {
+        return false;
+    }
+
+    return true;
+}
+#endif // NOT(ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PARSER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PARSER_STATIC_IMPLEMENTATION
+static
+#endif 
+bool ascii_isValidHexadecimalChar(char c)
+#ifdef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+    if(
+        ( (c >= '0') && (c <= '9') )
+        || ( (c >= 'a') && (c <= 'f') )
+        || ( (c >= 'A') && (c <= 'F') )
+    )
+    {
+        return true;
+    }
+
+    return false;
+}
+#endif // NOT(ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PARSER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PARSER_STATIC_IMPLEMENTATION
+static
+#endif 
+bool ascii_isValidAlphaNumericChar(char c)
+#ifdef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+    if(
+        ( (c >= '0') && (c <= '9') )
+        || ( (c >= 'a') && (c <= 'z') )
+        || ( (c >= 'A') && (c <= 'Z') )
+    )
+    {
+        return true;
+    }
+
+    return false;
+}
+#endif // NOT(ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PARSER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PARSER_STATIC_IMPLEMENTATION
+static
+#endif 
+bool ascii_onlyContainsValidDecimalChars(const char * str)
+#ifdef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+    while ( true )
+    {
+        if( !ascii_isValidDecimalChar(*str) )
+        {
+            break;
+        }
+        str++;
+    }
+    
+    return (*str == '\0');
+}
+#endif // NOT(ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PARSER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PARSER_STATIC_IMPLEMENTATION
+static
+#endif 
+bool ascii_onlyContainsValidHexadecimalChars(const char * str)
+#ifdef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+    while ( true )
+    {
+        if( !ascii_isValidHexadecimalChar(*str) )
+        {
+            break;
+        }
+        str++;
+    }
+    
+    return (*str == '\0');
+}
+#endif // NOT(ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PARSER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PARSER_STATIC_IMPLEMENTATION
+static
+#endif 
+bool ascii_onlyContainsValidBinaryChars(const char * str)
+#ifdef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+    while ( true )
+    {
+        if( !ascii_isValidBinaryChar(*str) )
+        {
+            break;
+        }
+        str++;
+    }
+    
+    return (*str == '\0');
+}
+#endif // NOT(ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PARSER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PARSER_STATIC_IMPLEMENTATION
+static
+#endif 
+bool ascii_onlyContainsValidAlphaNumericChars(const char * str)
+#ifdef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+    while ( true )
+    {
+        if( !ascii_isValidBinaryChar(*str) )
+        {
+            break;
+        }
+        str++;
+    }
+    
+    return (*str == '\0');
+}
+#endif // NOT(ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PARSER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PARSER_STATIC_IMPLEMENTATION
+static
+#endif 
+unsigned char ascii_hexChar2UnsignedValue(char input)
+#ifdef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+    if( (input >= 'A') && (input <= 'Z') )
+    {
+        return input - ('A'- 0xA);
+    }
+    else if ( (input >= 'a') && (input <= 'z') )
+    {
+        return input - ('a'- 0xA);
+    }
+    else if ( (input >= '0') && (input <= '9') )
+    {
+        return input - ('0');
+    }
+    else
+    {
+        return 0xFF;
+    }
+}
+#endif // NOT(ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PARSER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PARSER_STATIC_IMPLEMENTATION
+static
+#endif 
+unsigned char ascii_parseByteHex(const char *str)
+#ifdef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{  
+    unsigned char result = ascii_hexChar2UnsignedValue(*str);
+    if(ascii_isValidHexadecimalChar(*(++str)))
+    {
+        result <<= 4;
+        result += ascii_hexChar2UnsignedValue(*str);
+    }
+    return result;
+}
+#endif // NOT(ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PARSER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PARSER_STATIC_IMPLEMENTATION
+static
+#endif 
+unsigned int ascii_parseUnsignedHex(const char * str)
+#ifdef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+    unsigned int result = 0;
+    for (unsigned int i = 0; i < (sizeof(unsigned int)*2); i++)
+    {
+        if(!ascii_isValidHexadecimalChar(str[i]))
+           break;
+
+        result <<= 4;
+        result |= ascii_hexChar2UnsignedValue(str[i]);
+    }
+    return result;
+}
+#endif // NOT(ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PARSER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PARSER_STATIC_IMPLEMENTATION
+static
+#endif 
+unsigned int ascii_parseUnsignedDecimal(const char * str)
+#ifdef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+    unsigned int decMask = 1000000000; // 4,294,967,295 is unsigned int Max (2^32)
+    unsigned int result = 0;
+    char currentDigit = 0;
+
+    while (decMask)
+    {
+        currentDigit = *(str++);
+
+        if(!ascii_isValidDecimalChar(currentDigit))
+            break;
+
+        result *=10;
+
+        //Offset ASCI
+        currentDigit -= '0';
+
+        result += currentDigit;
+    }
+    return result;
+}
+#endif // NOT(ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PARSER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PARSER_STATIC_IMPLEMENTATION
+static
+#endif 
+int ascii_parseSignedDecimal(const char * str)
+#ifdef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+  int signum = 1;
+  if(*str == '-')
+  {
+      signum = -1;
+      str++;
+  }
+  if(*str == '+')
+  {
+      str++;
+  }
+  return signum * ascii_parseUnsignedDecimal(str);
+}
+#endif // NOT(ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION)
---
+++ test/asciiParserTest.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <time.h>
+
+/*****************************TEMPLATE INCLUDE**************************************/
+#define ASCII_PARSER_INLINE_IMPLEMENTATION
+#define ASCII_PARSER_STATIC_IMPLEMENTATION
+
+//following just for testing
+#define ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+#include "asciiParser.h" //Prototype
+#undef ASCII_PARSER_ONLY_PROTOTYPE_DECLARATION
+#include "asciiParser.h" //Implementation
+/***********************************************************************************/
+int main(void)
+{
+    clock_t startTime = clock();
+    
+    assert(0xCAFECAFE ==  ascii_parseUnsignedHex("cafecafe"));
+    assert(0xCAFE ==  ascii_parseUnsignedHex("cafe"));
+    assert(0x1234 ==  ascii_parseUnsignedHex("1234"));
+    assert(0xaBc9 ==  ascii_parseUnsignedHex("aBc9"));
+
+
+    assert(12345 ==  ascii_parseUnsignedDecimal("12345"));
+    assert(99999 ==  ascii_parseUnsignedDecimal("99999"));
+    assert(0 ==  ascii_parseUnsignedDecimal("000000"));
+    assert(9 ==  ascii_parseUnsignedDecimal("000009"));
+
+
+    assert(-99999 ==  ascii_parseSignedDecimal("-99999"));
+    assert(99999 ==  ascii_parseSignedDecimal("+99999"));
+    assert(99999 ==  ascii_parseSignedDecimal("99999"));
+    assert(0 ==  ascii_parseSignedDecimal("-0"));
+    assert(0 ==  ascii_parseSignedDecimal("+0"));
+    assert(0 ==  ascii_parseSignedDecimal("0"));
+
+    assert(0x0a == ascii_parseByteHex("0A"));
+    assert(0xa0 == ascii_parseByteHex("a0"));
+    assert(0xff == ascii_parseByteHex("ff"));
+
+    assert(0xcf == ascii_parseByteHex("cf"));
+    assert(0xfc == ascii_parseByteHex("fc"));
+
+    double elapsedTime = (double)(clock() - startTime) / CLOCKS_PER_SEC;
+    printf("Done in %f seconds\n", elapsedTime);
+}
